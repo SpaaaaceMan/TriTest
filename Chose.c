9b4d022c5b55f86b0e261ce7f4e69c7168ec7afd
RFOyashiro <kurt.savio+git@hotmail.fr>
2016-09-14 18:45:29 +0200

    Add files via upload
    Ecriture sur fichier + ordre de calcul + Détails dans .txt

---
+++ Chose.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main ()
+{
+	FILE * ResultTest = fopen("ResultatTestTri.csv", "w");
+	if (ResultTest == NULL)
+	{
+		printf("Impossible d'ouvrir le fichier\n");
+		return 1;
+	}
+	char * NomTest = "BlaBla";
+	int Temps[4];
+	Temps[1] = 2;
+	Temps[2] = 3;
+	Temps[3] = 7;
+	int Moyenne = 4;
+	
+	//Faire un test
+	//Enreistrement de chaque temps dans un tableau Temps
+	//Vérification que le temps total est < 5min (ajout du précédent temps dans une variable)
+	//Calcul de la moyenne à la fin
+	//Ecrire les résultats ensuite
+	fprintf(ResultTest, "%s;%d;%d;%d;%d\n",NomTest, Temps[1], Temps[2], Temps[3], Moyenne);
+	
+	return 0;
+}
