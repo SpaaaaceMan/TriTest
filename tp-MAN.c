e2e821340533139f01ad84524a15c86d1dc6540c
RFOyashiro <kurt.savio+git@hotmail.fr>
2016-09-21 16:43:59 +0200

    Add files via upload
    Csv Complet

--- tp-MAN.c
+++ tp-MAN.c
@@ -62,6 +62,68 @@ void sequentialInsertionSort (int tab[], int size)
 	}
 }
 
+void CreateCSV ()
+{
+	FILE * ResultTest = fopen("ResultatTestTri.csv", "w");
+	
+	if (ResultTest == NULL)
+	{
+		printf("Impossible d'ouvrir le fichier\n");
+		exit(1);
+	}
+	
+	char * Titre[23];
+	
+	Titre[0] = "Nom du tri";
+	Titre[1] = "Taille Tableau";
+	Titre[2] = "Temps Test 1";
+	Titre[3] = "Temps Test 2";
+	Titre[4] = "Temps Test 3";
+	Titre[5] = "Temps Test 4";
+	Titre[6] = "Temps Test 5";
+	Titre[7] = "Temps Test 6";
+	Titre[8] = "Temps Test 7";
+	Titre[9] = "Temps Test 8";
+	Titre[10] = "Temps Test 9";
+	Titre[11] = "Temps Test 10";
+	Titre[12] = "Temps Test 11";
+	Titre[13] = "Temps Test 12";
+	Titre[14] = "Temps Test 13";
+	Titre[15] = "Temps Test 14";
+	Titre[16] = "Temps Test 15";
+	Titre[17] = "Temps Test 16";
+	Titre[18] = "Temps Test 17";
+	Titre[19] = "Temps Test 18";
+	Titre[20] = "Temps Test 19";
+	Titre[21] = "Temps Test 20";
+	Titre[22] = "Moyenne";
+	
+	fprintf(ResultTest, "%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n",
+		Titre[0], Titre[1], Titre[2], Titre[3], Titre[4], Titre[5], Titre[6], Titre[7], Titre[8], Titre[9], 
+		Titre[10], Titre[11], Titre[12], Titre[13], Titre[14], Titre[15], Titre[16], Titre[17], Titre[18], Titre[19], 
+		Titre[20], Titre[21], Titre[22]);
+		
+	fclose(ResultTest);
+}
+
+FILE* OpenCSV ()
+{
+	FILE * ResultTest = fopen("ResultatTestTri.csv", "a");
+	
+	if (ResultTest == NULL)
+	{
+		printf("Impossible d'ouvrir le fichier\n");
+		exit(1);
+	}
+	
+	return ResultTest;
+}
+
+void CloseCSV (FILE * ResultTest)
+{
+	fclose(ResultTest);
+}
+
 int main ()
 {
     clock_t begin, end;
