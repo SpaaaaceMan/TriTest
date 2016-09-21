3dca1c23c6f01c00127355c56dcee7b81bb41c4f
SpaaaaceMan <julien7397@hotmail.fr>
2016-09-21 16:42:51 +0200

    Add files via upload

--- tp-MAN.c
+++ tp-MAN.c
@@ -1,3 +1,14 @@
+/**
+ * \file tp-MAN.c
+ * \brief Programme de tests.
+ * \author Julien TEULLE
+ * \version 1
+ * \date 14/09/16
+ *
+ * Programme de test pour divers tri
+ *
+ */
+
 #include "stdlib.h"
 #include "time.h"
 #include "stdio.h"
@@ -5,36 +16,63 @@
 #define MAX 1000000
 typedef int TABLEAU[MAX];
 
-void remplirTableauAleatoire (int tableau[], int taille)
+/**
+ * \fn fillWithRandomNumbers (int tab[], int size)
+ * \brief Fill a table with numbers randomly generated.
+ * \param tab The table which is filled.
+ * \param size The size of tab.
+ */
+void fillWithRandomNumbers (int tab[], int size)
 {
 	srand(time(NULL));
 	int i = 0;
-	for (; i < taille; i++)
+	for (; i < size; i++)
 	{
-		tableau[i] = (int) rand();
+		tab[i] = (int) rand() % 100;
 	}
 }
 
-void afficherTableau (int tableau[], int taille)
+/**
+ * \fn display (int tab[], int size)
+ * \brief Show the content of a table with one element per line.
+ * \param tab The table which is display.
+ * \param size The size of tab.
+ */
+void display (int tab[], int size)
 {
     int i = 0;
-	for (; i < taille; i++)
+	for (; i < size; i++)
     {
-        printf("%d %d\n", i, tableau[i]);
+        printf("%d %d\n", i, tab[i]);
     }
 }
 
-
+void sequentialInsertionSort (int tab[], int size)
+{
+	int i, p, x;
+	for (i = 1; i < size; i++)
+	{
+		p = 0;
+		while (tab[p] < tab[i]) p++;
+		x = tab[i];
+		int j;
+		for (j = i - 1; p <= j; j--)
+			tab[j+1] = tab[j];
+		tab[p] = x;
+	}
+}
 
 int main ()
 {
-    clock_t debut, fin;
-    debut = clock();
-    int taille = 1000;
-    int tab[taille];
-    remplirTableauAleatoire(tab, taille);
-    //afficherTableau(tab, taille);
-    fin = clock();
-    printf("%f", (((double)(fin - debut)) / CLOCKS_PER_SEC) * 1000.0);
+    clock_t begin, end;
+    begin = clock();
+    int size = 10;
+    int tab[size];
+    fillWithRandomNumbers(tab, size);
+    display(tab, size);
+    sequentialInsertionSort(tab, size);
+    display(tab, size);
+    end = clock();
+    printf("Execution time : %fms", (((double)(end - begin)) / CLOCKS_PER_SEC) * 1000.0);
     return 0;
 }
